d0953c4782968dbe9616b11952d64d8ee566b13c
Michael Oyelami <michaeloyelami@gmail.com>
2025-02-13 11:59:00 +0100

    includes basic caching

--- URLShortening.Application/Services/UrlShorteningService.cs
+++ URLShortening.Application/Services/UrlShorteningService.cs
@@ -1,10 +1,11 @@
-﻿using System.Net.Quic;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Web;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using URLShortening.Application.Contracts;
 using URLShortening.Application.Interfaces;
 using URLShortening.Application.ViewModels;
+using URLShortening.Domain;
 using URLShortening.Domain.Constants;
 using URLShortening.Domain.Entities;
 
@@ -15,11 +16,14 @@ namespace URLShortening.Application.Services
         private readonly IShortenedUrlRepository ShortenedUrlRepository;
         private readonly ILogger<UrlShorteningService> Logger;
         private readonly Random _random = new();
+        private IMemoryCache Cache;
+        private const string codeCacheKey = "code";
 
-        public UrlShorteningService(IShortenedUrlRepository shortenedUrlRepository, ILogger<UrlShorteningService> logger)
+        public UrlShorteningService(IShortenedUrlRepository shortenedUrlRepository, ILogger<UrlShorteningService> logger, IMemoryCache cache)
         {
             ShortenedUrlRepository = shortenedUrlRepository ?? throw new ArgumentNullException(nameof(shortenedUrlRepository));
             Logger = logger;
+            Cache = cache ?? throw new ArgumentNullException(nameof(cache));
         }
 
         private async Task<string> GenerateUniqueCode()
@@ -60,6 +64,36 @@ namespace URLShortening.Application.Services
             return urlParts[urlParts.Length - 1];
         }
 
+        private async Task<RetrieveUrlResponse> UpdateUrl(string code)
+        {
+            var response = new RetrieveUrlResponse { Success = false };
+            var shortenedUrl = await ShortenedUrlRepository.GetFirstOrDefault(s => s.Code == code);
+            if (shortenedUrl is null)
+            {
+                response.ErrorReason = ErrorConstants.SHORTENED_URL_DOES_NOT_EXIST;
+                return response;
+            }
+            shortenedUrl.HitCount++;
+            var data = await ShortenedUrlRepository.Update(shortenedUrl);
+            if (data is null)
+            {
+                response.ErrorReason = ErrorConstants.SHORTENED_URL_UPDATE_FAILED;
+                return response;
+            }
+
+            Cache.Remove($"{codeCacheKey}:{code}");
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(3600))
+                    .SetPriority(CacheItemPriority.Normal)
+                    .SetSize(1024);
+            Cache.Set($"{codeCacheKey}:{code}", data, cacheEntryOptions);
+
+            response.LongUrl = shortenedUrl.LongUrl;
+            response.Success = true;
+            return response;
+        }
+
         public async Task<ShortenUrlResponse> ShortenUrl(ShortenUrlRequest request)
         {
             Logger.LogInformation("Executing {Action} with parameters: {Parameters}", nameof(ShortenUrl), JsonSerializer.Serialize(request));
@@ -98,20 +132,21 @@ namespace URLShortening.Application.Services
             Logger.LogInformation("Executing {Action} with parameters: {Parameters}", nameof(ShortenUrl), shortUrl);
             var response = new RetrieveUrlResponse { Success = false };
             var code = GetCode(shortUrl);
-            var shortenedUrl = await ShortenedUrlRepository.GetFirstOrDefault(s => s.Code == code);
-            if (shortenedUrl is null)
+
+            // In-Memory Caching
+            if (Cache.TryGetValue($"{codeCacheKey}:{code}", out ShortenedUrl? url))
             {
-                response.ErrorReason = ErrorConstants.SHORTENED_URL_DOES_NOT_EXIST;
-                return response;
+                Logger.LogInformation($"Code: {code} found in cache.");
+                response.LongUrl = url?.LongUrl;
+                await UpdateUrl(code);
             }
-            shortenedUrl.HitCount++;
-            var data = await ShortenedUrlRepository.Update(shortenedUrl);
-            if (data is null)
+            else
             {
-                response.ErrorReason = ErrorConstants.SHORTENED_URL_UPDATE_FAILED;
-                return response;
+                Logger.LogInformation("Code not found in cache. Fetching from database.");
+                var updatedUrl = await UpdateUrl(code);
+                response.LongUrl = updatedUrl.LongUrl;
             }
-            response.LongUrl = shortenedUrl.LongUrl;
+
             response.Success = true;
             return response;
         }
